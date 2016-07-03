b8a0a709b5293faef3990bf4291113b1616e8cbd
Marcello Duarte <marcello.duarte@gmail.com>
2016-07-03 20:36:28 +0100

    Add concat

--- src/Md/Phunkie/Functions/immlist.php
+++ src/Md/Phunkie/Functions/immlist.php
@@ -10,3 +10,16 @@ function tail(ImmList $list) { return $list->tail(); }
 function last(ImmList $list) { return $list->last(); }
 function reverse(ImmList $list) { return $list->reverse(); }
 function length(ImmList $list) { return $list->length; }
+
+function concat(...$items)
+{
+    $result = [];
+    foreach ($items as $item) {
+        if (!$item instanceof ImmList) {
+            $result[] = $item;
+        } else {
+            $result = array_merge($result, $item->toArray());
+        }
+    }
+    return ImmList(...$result);
+}
