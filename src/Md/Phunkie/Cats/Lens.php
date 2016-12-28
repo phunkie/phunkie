475e00216e9a19ca96aa3516e5d5be7a0d82ed96
Marcello Duarte <marcello.duarte@gmail.com>
2016-12-28 15:13:28 +0000

    Remove phpdocs from lens as it is messing up with the IDE

--- src/Md/Phunkie/Cats/Lens.php
+++ src/Md/Phunkie/Cats/Lens.php
@@ -2,48 +2,27 @@
 
 namespace Md\Phunkie\Cats;
 
-/**
- * Lens<A,B>
- */
 class Lens
 {
     private $g;
     private $s;
 
-    /**
-     * @param callable<A, B> $g
-     * @param callable<(A,B), A> $s
-     */
     public function __construct(callable $g, callable $s)
     {
         $this->g = $g;
         $this->s = $s;
     }
 
-    /**
-     * @param A $a
-     * @return B
-     */
     public function get($a)
     {
         return call_user_func($this->g, $a);
     }
 
-    /**
-     * @param B $b
-     * @param A $a
-     * @return A
-     */
     public function set($b, $a)
     {
         return call_user_func_array($this->s, [$b, $a]);
     }
 
-    /**
-     * @param callable<B,B> $f
-     * @param A $a
-     * @return A
-     */
     public function mod(callable $f, $a)
     {
         return $this->set($f($this->get($a)), $a);
