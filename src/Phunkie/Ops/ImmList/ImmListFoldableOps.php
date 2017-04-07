44e83aeb8c9d90f5593ce630a5fdc5e50a077b47
Marcello Duarte <marcello.duarte@gmail.com>
2017-04-07 09:30:16 +0100

    Refactor ImmList fold to use Trampoline

--- src/Phunkie/Ops/ImmList/ImmListFoldableOps.php
+++ src/Phunkie/Ops/ImmList/ImmListFoldableOps.php
@@ -15,26 +15,35 @@ use function Phunkie\Functions\semigroup\combine;
 use function Phunkie\Functions\semigroup\zero;
 use Phunkie\Types\ImmList;
 use function Phunkie\Functions\currying\applyPartially;
+use Phunkie\Utils\Trampoline\Done;
+use Phunkie\Utils\Trampoline\More;
+use Phunkie\Utils\Trampoline\Trampoline;
 
 trait ImmListFoldableOps
 {
     public function foldLeft($initial)
     {
         return applyPartially([$initial], func_get_args(), function(callable $f) use ($initial) {
-            $acc = function(ImmList $xs, $initial) use (&$acc, $f) {
-                return $xs->isEmpty() ? $initial : $acc($xs->tail(), $f($initial, $xs->head()));
+            $acc = function(ImmList $xs, $initial) use (&$acc, $f): Trampoline {
+                return $xs->isEmpty() ? new Done($initial) :
+                    new More(function() use ($acc, $f, $xs, $initial) {
+                        return $acc($xs->tail(), $f($initial, $xs->head()));
+                    });
             };
-            return $acc($this, $initial);
+            return $acc($this, $initial)->run();
         });
     }
 
     public function foldRight($initial)
     {
         return applyPartially([$initial], func_get_args(), function(callable $f) use ($initial) {
-            $acc = function (ImmList $xs, $initial) use (&$acc, $f) {
-                return $xs->isEmpty() ? $initial : $acc($xs->init(), $f($xs->last(), $initial));
+            $acc = function (ImmList $xs, $initial) use (&$acc, $f): Trampoline {
+                return $xs->isEmpty() ? new Done($initial) :
+                    new More(function() use ($acc, $f, $xs, $initial) {
+                        return $acc($xs->init(), $f($xs->last(), $initial));
+                    });
             };
-            return $acc($this, $initial);
+            return $acc($this, $initial)->run();
         });
     }
 
