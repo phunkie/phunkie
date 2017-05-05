c9024680d8d0d5cb2107496645b450cbe505cee0
Marco Perone <m.perone@mvlabs.it>
2017-05-05 08:34:18 +0200

    ImmSet is now Applicative

--- spec/Phunkie/Types/ImmSetSpec.php
+++ spec/Phunkie/Types/ImmSetSpec.php
@@ -4,14 +4,24 @@ namespace spec\Phunkie\Types;
 
 use PhpSpec\ObjectBehavior;
 use Phunkie\Cats\Functor;
+use function Phunkie\Functions\applicative\ap;
+use function Phunkie\Functions\applicative\pure;
+use function Phunkie\Functions\applicative\map2;
 use function Phunkie\Functions\functor\allAs;
 use function Phunkie\Functions\functor\asVoid;
 use function Phunkie\Functions\functor\fmap;
 use function Phunkie\Functions\functor\zipWith;
 use const Phunkie\Functions\numbers\increment;
+use Phunkie\Types\ImmSet;
+
+use Eris\TestTrait;
+use Eris\Generator\SequenceGenerator as SeqGen;
+use Eris\Generator\IntegerGenerator as IntGen;
 
 class ImmSetSpec extends ObjectBehavior
 {
+    use TestTrait;
+
     function it_is_initializable()
     {
         $this->shouldHaveType('Phunkie\Types\ImmSet');
@@ -100,6 +110,35 @@ class ImmSetSpec extends ObjectBehavior
         expect(zipWith(function($x) { return 2 * $x; }, ImmSet(1,2,3)))->toBeLike(
             ImmSet(Pair(1,2), Pair(2,4), Pair(3,6))
         );
+    }
+
+    function it_is_an_applicative()
+    {
+        $xs = (ap (ImmSet(function($a) { return $a +1; }))) (ImmSet(1));
+        expect($xs)->toBeLike(ImmSet(2));
+
+        $xs = (pure (ImmSet)) (42);
+        expect($xs)->toBeLike(ImmSet(42));
+
+        $xs = ((map2 (function($x, $y) { return $x + $y; })) (ImmSet(1))) (ImmSet(2));
+        expect($xs)->toBeLike(ImmSet(3));
+    }
+
+    function it_returns_an_empty_set_when_an_empty_set_is_applied()
+    {
+        $this->beAnInstanceOf(ImmSet::class);
+        $this->beConstructedWith();
+        $this->apply(ImmSet(function ($x) {return $x + 1;}))->shouldBeEmpty();
+    }
+
+    function it_applies_the_result_of_the_function_to_a_Set()
+    {
+        $this->forAll(
+            new SeqGen(new IntGen())
+        )->then(function($list) {
+            expect(ImmSet(...$list)->apply(ImmSet(function($x) { return $x + 1; })))
+                ->toBeLike(ImmSet(...array_map(function($x) { return $x + 1; }, $list)));
+        });
     }
 }
 
--- src/Phunkie/Functions/common.php
+++ src/Phunkie/Functions/common.php
@@ -13,6 +13,7 @@ array_map (function($file) { require_once $file; }, glob(__DIR__ .'/*'));
 
 const Option = 'Option';
 const ImmList = 'ImmList';
+const ImmSet = 'ImmSet';
 const Function1 = 'Function1';
 const _ = "Phunkie@Reserverd@Constant@_";
 const None = 'Phunkie@Reserverd@Constant@None';
